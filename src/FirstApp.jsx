fc8c5604553f7b8086d21923e534a21d63877d55
Alejandro Salgado <alejandrosram@outlook.com>
2022-06-22 11:40:15 -0500

    Update counter app

---
+++ src/FirstApp.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+export const FirstApp = ({ title, subtitle, name }) => {
+	return (
+		<>
+			<h1>{title}</h1>
+			<p>{subtitle}</p>
+			<p>{name}</p>
+		</>
+	);
+};
+
+FirstApp.propTypes = {
+	name: PropTypes.string,
+	subtitle: PropTypes.string,
+	title: PropTypes.string.isRequired,
+};
+
+FirstApp.defaultProps = {
+	name: "Alex",
+	subtitle: "No hay subtítulo",
+	title: "No hay título",
+};
